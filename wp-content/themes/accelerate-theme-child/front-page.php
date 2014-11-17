956bb9f5a4e0baac942a5c741afbe838c835fd3a
Balance Media <adda@balance.coop>
2014-11-17 17:00:00 -0500

    adding case studies to the homepage

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -25,6 +25,30 @@ get_header(); ?>
 	</div><!-- .site-content -->
 </section><!-- .home-page -->
 
+<section class="featured-work">
+	<div class="site-content">
+		<h4>Featured Work</h4>
+
+		<ul class="homepage-featured-work">
+		<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
+			<?php while ( have_posts() ) : the_post(); 
+				$image_1 = get_field("image_1");
+				$size = "medium";
+			?>
+			<li class="individual-featured-work">
+				<figure>
+					<?php echo wp_get_attachment_image($image_1, $size); ?>
+				</figure>
+
+				<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+			</li>
+			<?php endwhile; // end of the loop. ?>
+			<?php wp_reset_query(); // resets the altered query back to the original ?>
+		</ul>
+
+	</div>
+</section>
+
 <section class="recent-posts">
 	<div class="site-content">
 		<div class="blog-post">
