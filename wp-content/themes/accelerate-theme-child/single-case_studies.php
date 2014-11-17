2ec3662fcd33a541e69c9ee15a38d390c3b60c67
Balance Media <adda@balance.coop>
2014-11-17 13:18:46 -0500

    better image stuffs

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -19,7 +19,9 @@ get_header(); ?>
 		$link = get_field('site_link');
 		$image_1 = get_field('image_1');
 		$image_2 = get_field('image_2');
-		$image_3 = get_field('image_3'); ?>
+		$image_3 = get_field('image_3'); 
+		$size = "full";
+		?>
 
 	<article class="case-study">
 		<aside class="case-study-sidebar">
@@ -33,19 +35,16 @@ get_header(); ?>
 		</aside>
 
 		<div class="case-study-images">
-			<?php if($image_1) { ?>
-				<img src="<?php echo $image_1; ?>" />
-			<?php } ?>
-			
-			<?php if($image_2) { ?>
-				<img src="<?php echo $image_2; ?>" />
-			<?php } ?>
-
-			<?php if($image_3) { ?>
-			<img src="<?php echo $image_3; ?>" />
-			<?php } ?>
+			<?php if($image_1) {
+				echo wp_get_attachment_image( $image_1, $size );
+			} ?>
+			<?php if($image_2) { 
+				echo wp_get_attachment_image( $image_2, $size );
+			} ?>
+			<?php if($image_3) {
+				echo wp_get_attachment_image( $image_3, $size );
+			} ?>
 		</div>
-
 
 			</article>
 			<?php endwhile; // end of the loop. ?>
