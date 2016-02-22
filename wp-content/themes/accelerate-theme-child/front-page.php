7edf3188a060cb8f242536190f8ee4daced0cf03
Denise <denise72214@outlook.com>
2016-02-22 10:22:54 -0700

    added child and query

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -13,7 +13,6 @@
  */
 
 get_header(); ?>
-    <pre><?php print_r($wp_query); exit; ?></pre>
     <section class="home-page">
         <div class="site-content">
             <?php while ( have_posts() ) : the_post(); ?>
@@ -25,6 +24,23 @@ get_header(); ?>
         </div>
         <!-- .container -->
     </section>
+
+    <section class="recent-posts">
+        <div class="site-content">
+            <div class="blog-post">
+                <h4>From the Blog</h4>
+                <?php query_posts('posts_per_page=1'); ?>
+                    <?php while ( have_posts() ) : the_post(); ?>
+                        <h2><?php the_title(); ?></h2>
+<?php the_excerpt(); ?>
+    <a href="<?php the_permalink(); ?>" class="read-more-link">Read More <span>&rsaquo;</span></a>
+                    <?php endwhile; // end of the loop. ?>
+                        <?php wp_reset_query(); // resets the altered query back to the original ?>
+            </div>
+        </div>
+    </section>
+
+
     <!-- .home-page -->
 
     <?php get_footer(); ?>
--- wp-content/themes/accelerate-theme/index.php
+++ wp-content/themes/accelerate-theme/index.php
@@ -15,44 +15,47 @@
  */
 
 get_header(); ?>
-	<!-- BLOG PAGE -->
-	<section class="blog-page">
-		<div class="site-content">
-			<div class="main-content">
-				
-				<?php
+    
+    <!-- BLOG PAGE -->
+    <section class="blog-page">
+        <div class="site-content">
+            <div class="main-content">
+
+                <?php
 			if ( have_posts() ) :
 				// Start the Loop.
 				while ( have_posts() ) : the_post(); ?>
-				
-					<article class="post-entry">
-					<div class="entry-wrap">
-						<header class="entry-header">
-							<div class="entry-meta">
-								<time class="entry-time" datetime="2014-09-29T04:33:51+00:00" itemprop="datePublished" title="Monday, September 29, 2014, 4:33 am"><?php the_date();?></time>
-							</div>
-							<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-						</header>
-						<div class="entry-summary">
-							<?php the_excerpt(); ?>
-						</div>
-						<footer class="entry-footer">
-							<div class="entry-meta">
-								<span class="entry-terms author">Written by <a href=""><?php the_author(); ?></a></span>
-								<span class="entry-terms category">Posted in <a href="">Books</a></span>
-								<span class="entry-terms commets"><a href="">2 Comments</a></span>
-							</div>
-						</footer>
-					</div>
-				</article>
-			<?php endwhile; endif; ?>
 
-			</div>
-			
-			<?php get_sidebar(); ?>
-		</div>
-				<div class="clearfix"></div>
+                    <article class="post-entry">
+                        <div class="entry-wrap">
+                            <header class="entry-header">
+                                <div class="entry-meta">
+                                    <time class="entry-time" datetime="2014-09-29T04:33:51+00:00" itemprop="datePublished" title="Monday, September 29, 2014, 4:33 am">
+                                        <?php the_date();?>
+                                    </time>
+                                </div>
+                                <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                            </header>
+                            <div class="entry-summary">
+                                <?php the_excerpt(); ?>
+                            </div>
+                            <footer class="entry-footer">
+                                <div class="entry-meta">
+                                    <span class="entry-terms author">Written by <a href=""><?php the_author(); ?></a></span>
+                                    <span class="entry-terms category">Posted in <a href="">Books</a></span>
+                                    <span class="entry-terms commets"><a href="">2 Comments</a></span>
+                                </div>
+                            </footer>
+                        </div>
+                    </article>
+                    <?php endwhile; endif; ?>
 
-	</section>
-	<!-- END blog page -->
-<?php get_footer();
+            </div>
+
+            <?php get_sidebar(); ?>
+        </div>
+        <div class="clearfix"></div>
+
+    </section>
+    <!-- END blog page -->
+    <?php get_footer();
