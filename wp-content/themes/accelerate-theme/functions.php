4df90a5ebf95f3736100602f72d78469fc9cc909
Ann Cascarano <a.cascarano@gmail.com>
2016-03-14 12:15:17 -0400

    Adds widget area for blog sidebar, functions cleanup
    and .htaccess to .gitignore file.

--- wp-content/themes/accelerate-theme/functions.php
+++ wp-content/themes/accelerate-theme/functions.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Accelerate Marketing Child functions and definitions
+ * Accelerate Marketing functions and definitions
  *
  * Set up the theme and provides some helper functions, which are used in the
  * theme as custom template tags. Others are attached to action and filter
@@ -25,6 +25,29 @@
  * @since Accelerate Marketing 1.0
  */
 
-register_nav_menu('top-nav', 'Top Nav');
-register_nav_menu('social-media', 'Social Media Nav');
+/**
+ * Register menus.
+ */
+function accelerate_setup() {
+  register_nav_menus( array(
+    'top-nav' => __( 'Top Nav', 'accelerate' ),
+    'social-media'  => __( 'Social Media Nav', 'accelerate' ),
+  ) );
+}
+add_action( 'after_setup_theme', 'accelerate_setup' ); 
 
+/**
+ * Register widget area.
+ */
+function accelerate_widgets_init() {
+	register_sidebar( array(
+		'name'          => __( 'Widget Area', 'accelerate' ),
+		'id'            => 'sidebar-1',
+		'description'   => __( 'Add widgets here to appear in your blog sidebar.', 'accelerate' ),
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+}
+add_action( 'widgets_init', 'accelerate_widgets_init' );
