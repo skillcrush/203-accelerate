42084d6d0b6c67f9cbbc2dd8801cc9a721cb6970
Sara Bohannon <sarabohannon@gmail.com>
2016-05-29 05:36:43 -0500

    forgot to add and commit changes?

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -14,7 +14,6 @@
 
 get_header(); ?>
 
-
 <section class="home-page">
 	<div class="site-content">
 		<?php while ( have_posts() ) : the_post(); ?>
@@ -23,10 +22,34 @@ get_header(); ?>
 				<a class="button" href="<?php echo home_url(); ?>/blog">View Our Work</a>
 			</div>
 		<?php endwhile; // end of the loop. ?>
-	</div><!-- .container -->
+	</div><!-- .site-content -->
 </section><!-- .home-page -->
 
-</section class="recent-posts">
+<section class="featured-work">
+	<div class="site-content">
+		<h4>Featured Work</h4>
+
+		<ul class="homepage-featured-work">
+		<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
+			<?php while ( have_posts() ) : the_post(); 
+				$image_1 = get_field("image_1");
+				$size = "medium";
+			?>
+			<li class="individual-featured-work">
+				<figure>
+					<?php echo wp_get_attachment_image($image_1, $size); ?>
+				</figure>
+
+				<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+			</li>
+			<?php endwhile; // end of the loop. ?>
+			<?php wp_reset_query(); // resets the altered query back to the original ?>
+		</ul>
+
+	</div>
+</section>
+
+<section class="recent-posts">
 	<div class="site-content">
 		<div class="blog-post">
 			<h4>From the Blog</h4>
@@ -35,9 +58,9 @@ get_header(); ?>
 				<h2><?php the_title(); ?></h2>
 				<?php the_excerpt(); ?>
 				<a href="<?php the_permalink(); ?>" class="read-more-link">Read More <span>&rsaquo;</span></a>
-			<?php endwhile; //end of the loop. ?>
+			<?php endwhile; // end of the loop. ?>
 			<?php wp_reset_query(); // resets the altered query back to the original ?>
-		</div>	
+		</div>
 	</div>
 </section>
 
