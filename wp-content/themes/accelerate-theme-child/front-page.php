d706cbc4c07064b7666498e3bedef6c9c060e694
crazycodegirl <joannesullivan69@yahoo.com>
2017-02-16 21:07:56 -0700

    Site Backup
    Includes all initial changes from Donna

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -13,7 +13,7 @@
  */
 
 get_header(); ?>
-<pre><?php print_r($wp_query); exit; ?></pre>
+
 
 <section class="home-page">
 	<div class="site-content">
@@ -24,6 +24,21 @@ get_header(); ?>
 			</div>
 		<?php endwhile; // end of the loop. ?>
 	</div><!-- .container -->
-</section><!-- .home-page -->
+</section>
+<section class="recent-posts">
+	<div class="site-content">
+		<div class="blog-post">
+	     <h4>From the Blog</h4>
+			 <?php query_posts('posts_per_page=1'); ?>
+			 	<?php while ( have_posts() ) : the_post(); ?>
+					<h2><?php the_title(); ?></h2>
+					<?php the_excerpt(); ?>
+					<a class="read-more-link" href="<?php the_permalink(); ?>">Read More <span>&rsaquo;</span></a>
+				<?php endwhile; ?>
+			<?php wp_reset_query(); ?>
+	  </div>
+	</div>
+</section>
+<!-- .home-page -->
 
 <?php get_footer(); ?>
