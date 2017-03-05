2c5c2845bc5218814ef1798903dde05fd547384f
Crystel1960 <Crystel1960@gmail.com>
2017-03-05 16:04:11 -0500

    added functions.php single_case_studies.php and uploads

---
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying case studies
+
+ *
+ * @package WordPress
+ * @subpackage Accelerate_Theme
+ * @since Accelerate theme 1.1
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content">
+		<div id="content" role="main">
+			<?php while ( have_posts() ) : the_post(); ?>
+        <article class="case-study">
+				<?php the_content(); ?>
+      </article>
+			<?php endwhile; // end of the loop. ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+
+<?php get_footer(); ?>
