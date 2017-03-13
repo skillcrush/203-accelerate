de08ac86343963a495eac62c2f457354779508c5
Crystel1960 <Crystel1960@gmail.com>
2017-03-12 20:23:20 -0400

    files updated

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -23,7 +23,33 @@ get_header(); ?>
 			</div>
 		<?php endwhile; // end of the loop. ?>
 	</div><!-- .container -->
-</section>
+</section><!-- homepage -->
+
+<section class="featured-work">
+ <div class="site-content">
+   <h4>Featured Work</h4>
+
+<ul class="homepage-featured-work">
+	 <?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
+		<?php while ( have_posts() ) : the_post();
+				$image_1 = get_field("image_1");
+				$size = "medium";
+				?>
+
+			<li class="individual-featured-work">
+			<figure>
+					<?php echo wp_get_attachment_image ($image_1, $size); ?>
+			</figure>
+
+			<h3<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+		</li>
+		<?php endwhile; // end of the loop. ?>
+	 <?php wp_reset_query(); // resets the altered query back to the original ?>
+	</ul>
+ </div>
+ </section><!--featured work -->
+
+
 <section class="recent-posts">
  <div class="site-content">
   <div class="blog-post">
@@ -31,9 +57,9 @@ get_header(); ?>
     <?php query_posts('posts_per_page=1'); ?>
      <?php while ( have_posts() ) : the_post(); ?>
        <h2><?php the_title(); ?></h2>
-       <?php the_excerpt(); ?> 
+       <?php the_excerpt(); ?>
        <a class="read-more-link" href="<?php the_permalink(); ?>">Read More <span>&rsaquo;</span></a>
-     <?php endwhile; ?> 
+     <?php endwhile; ?>
     <?php wp_reset_query(); ?>
    </div>
   </div>
--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -12,9 +12,37 @@ get_header(); ?>
 
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
-        <article class="case-study">
-				<?php the_content(); ?>
+
+			<?php while ( have_posts() ) : the_post();
+				$services = get_field('services');
+				$client = get_field('client');
+				$link = get_link('site_link');
+				$image_1 = get_field('image_1');
+				$image_2 = get_field('image_2');
+				$image_3 = get_field('image_3');?>
+
+			<article class="case-study">
+				<aside class="case-study-sidebar">
+				<h2><?php the_title(); ?></h2>
+				<h5><?php echo $services; ?></h5>
+				<h6><?php echo $client; ?></h6>
+
+					<?php the_content(); ?>
+
+					<p><strong><a href="<?php echo $link; ?>">Site Link</a></strong></p>
+				</aside>
+
+					<div class="case-study-images">
+					<?php if($image_1) { ?>
+						<?php echo wp_get_attachment_image ($image_1, $size); ?>
+						<?php } ?>
+						<?php if($image_2) { ?>
+							<?php echo wp_get_attachment_image ($image_2, $size); ?>
+							<?php } ?>
+							<?php if($image_3) { ?>
+								<?php echo wp_get_attachment_image ($image_3, $size); ?>
+								<?php } ?>
+					</div>
       </article>
 			<?php endwhile; // end of the loop. ?>
 
