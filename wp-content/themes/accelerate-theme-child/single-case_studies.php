f009f67d07df91b0f88734ea524e2f9019b0b845
crazycodegirl <joannesullivan69@yahoo.com>
2017-03-13 10:58:55 -0600

    SC-203 Tuesday
    Advanced Custom Fields added to case study template

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -14,11 +14,45 @@
 
 get_header(); ?>
 
-	<div id="primary" class="site-content">
-		<div id="content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
-				<article class="case-study">
-        	<?php the_content(); ?>
+<div id="primary" class="site-content">
+	<div id="content" role="main">
+
+	<?php while ( have_posts() ) : the_post();
+		$size = "full";
+		$services = get_field('services');
+		$client = get_field('client');
+		$link = get_field('site_link');
+		$image_1 = get_field('image_1');
+		$image_2 = get_field('image_2');
+		$image_3 = get_field('image_3'); ?>
+
+
+	<article class="case-study">
+		<aside class="case-study-sidebar">
+			<h2><?php the_title(); ?></h2>
+			<h5><?php echo $services; ?></h5>
+			<h6>Client: <?php echo $client; ?></h6>
+
+			<?php the_content(); ?>
+
+			<p><a href="<?php echo $link; ?>"><strong>Site Link</strong></a></p>
+		</aside>
+
+		<div class="case-study-images">
+			<?php
+			 if($image_1) {
+				echo wp_get_attachment_image( $image_1, $size);
+			 }
+
+			if($image_2) {
+				echo wp_get_attachment_image( $image_2, $size);
+			}
+
+			if($image_3) {
+				echo wp_get_attachment_image( $image_3, $size);
+			} ?>
+		</div>
+
 				</article>
 			<?php endwhile; // end of the loop. ?>
 
