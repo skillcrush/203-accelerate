b5e95e3faf7ef30e1a38513a5810a9d0dc8df36b
crazycodegirl <joannesullivan69@yahoo.com>
2017-03-13 09:52:57 -0600

    SC203 - Monday
    Created custom post type for case study pages

---
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 1.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content">
+		<div id="content" role="main">
+			<?php while ( have_posts() ) : the_post(); ?>
+				<article class="case-study">
+        	<?php the_content(); ?>
+				</article>
+			<?php endwhile; // end of the loop. ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
