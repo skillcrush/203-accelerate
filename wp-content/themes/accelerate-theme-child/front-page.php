2d0a1e6a9aa37ebf8f7935b45fabf2155f9bc238
rrturnbow <rarturnbow@gmail.com>
2017-07-05 16:09:24 -0600

    changes made to front-page.php

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -24,5 +24,19 @@ get_header(); ?>
 		<?php endwhile; // end of the loop. ?>
 	</div><!-- .container -->
 </section><!-- .home-page -->
+<section class="recent-posts"> <!--recent posts-->
+	<div class="site-content">
+		<div class="blog-post">
+			<h4>From the Blog</h4>
+			<?php query_posts('posts_per_page=1'); ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+				<h2><?php the_title(); ?></h2>
+				<?php the_excerpt(); ?>
+				<a class="read-more-link" href="<?php the_permalink(); ?>">Read More <span>&rsaquo;</span></a>
+			<?php endwhile; ?>
+			<?php wp_reset_query(); ?>
+		</div>
+	</div>
+</section>
 
 <?php get_footer(); ?>
