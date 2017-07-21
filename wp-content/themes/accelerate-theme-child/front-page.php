6a26522908e7f251b8d4f1386024843e81feb7a5
Maren Vernon <marenvernon@gmail.com>
2017-07-21 14:54:31 -0700

    small fix based on master fixes with revised query tk

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -14,21 +14,18 @@
 
 get_header(); ?>
 
-<section class="home-page">
-	<div class="site-content">
-		<?php while ( have_posts() ) : the_post(); ?>
-			<div class='homepage-hero'>
+	<div id="primary" class="home-page hero-content">
+		<div class="main-content" role="main">
+			<?php while ( have_posts() ) : the_post(); ?>
 				<?php the_content(); ?>
 				<a class="button" href="<?php echo home_url(); ?>/blog">View Our Work</a>
-			</div>
-		<?php endwhile; // end of the loop. ?>
-	</div><!-- .site-content -->
-</section><!-- .home-page -->
+			<?php endwhile; // end of the loop. ?>
+		</div><!-- .main-content -->
+	</div><!-- #primary -->
 
-<section class="recent-posts">
-	<div class="site-content">
+	<section class="recent-posts site-content clearfix">
 		<div class="blog-post">
-			<h4>From the Blog</h4>
+			<h3>From the Blog</h3>
 			<?php query_posts('posts_per_page=1'); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 				<h2><?php the_title(); ?></h2>
@@ -36,7 +33,6 @@ get_header(); ?>
 			<?php endwhile; // end of the loop. ?>
 			<?php wp_reset_query(); // resets the altered query back to the original ?>
 		</div>
-	</div>
-</section>
+	</section>
 
 <?php get_footer(); ?>
