ddb2f8350a0dad4908ad04fd487f20d4e31d825f
Maren Vernon <marenvernon@gmail.com>
2017-08-16 21:40:28 -0700

    recent posts fix

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -24,16 +24,18 @@ get_header(); ?>
 	</div><!-- #primary -->
 
 	<!-- RECENT BLOG POST -->
-	<section class="recent-posts site-content clearfix">
-		<div class="blog-post">
-			<h4>From the Blog</h4>
-			<?php query_posts('posts_per_page=1'); ?>
-				<!-- the loop -->
-				<?php while ( have_posts() ) : the_post(); ?>
-					<h3><?php the_title(); ?></h3>
-					<?php the_excerpt(); ?>
-				<?php endwhile; // end of the loop. ?>
-			<?php wp_reset_query(); // resets the altered query back to the original ?>
+	<section class="recent-posts">
+		<div class="site-content">
+			<div class="blog-post">
+				<h4>From the Blog</h4>
+				<?php query_posts('posts_per_page=1'); ?>
+					<!-- the loop -->
+					<?php while ( have_posts() ) : the_post(); ?>
+						<h3><?php the_title(); ?></h3>
+						<?php the_excerpt(); ?>
+					<?php endwhile; // end of the loop. ?>
+				<?php wp_reset_query(); // resets the altered query back to the original ?>
+			</div>
 		</div>
 	</section>
 
