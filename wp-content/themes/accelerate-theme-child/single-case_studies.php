14a33f6962298dc81a5ba539a4ea9e9323ec3671
Maren Vernon <marenvernon@gmail.com>
2017-08-28 06:59:49 -0700

    adding single case studies and acf plugins

---
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * The template for displaying a single case study.
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content">
+		<div class="main-content" role="main">
+			<?php while ( have_posts() ) : the_post();
+				$size = "full";
+				$services = get_field('services');
+				$client = get_field('client');
+				$site_link = get_field('site_link');
+				$image_1 = get_field('image_1');
+				$image_2 = get_field('image_2');
+				$image_3 = get_field('image_3'); ?>
+
+			<article class="case-study">
+					<aside class="case-study-sidebar">
+						<h2><?php the_title(); ?></h2>
+						<h4><span><?php echo $services; ?></span></h4>
+						<h4>Client: <?php echo $client; ?></h4>
+
+						<?php the_content(); ?>
+
+						<p class="read-more-link"><a href="<?php echo $site_link; ?>">Visit Live Site &rsaquo;</a></p>
+					</aside>
+
+					<div class="case-study-images">
+						<?php if($image_1){
+							echo wp_get_attachment_image( $image_1, $size );
+						} ?>
+
+						<?php if($image_2){
+							echo wp_get_attachment_image( $image_2, $size );
+						} ?>
+
+						<?php if($image_3){
+							echo wp_get_attachment_image( $image_3, $size );
+						} ?>
+					</div>
+				</article>
+
+			<?php endwhile; // end of the loop. ?>
+
+		</div><!-- .main-content -->
+	</div><!-- #primary -->
+
+	<div id="navigation" class="container">
+		<div class="left"><a href="<?php echo site_url('/case-studies/') ?>">&larr; <span>Back to Work</span></a></div>
+	</div>
+
+<?php get_footer(); ?>
