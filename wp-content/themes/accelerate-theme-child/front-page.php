2996066a3216562bfd11b1cd129341e218c10392
Maren Vernon <marenvernon@gmail.com>
2017-08-28 11:49:08 -0700

    adding featured work to homepage

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -23,6 +23,31 @@ get_header(); ?>
 		</div><!-- .main-content -->
 	</div><!-- #primary -->
 
+	<!-- FEATURED WORK -->
+	<section class="featured-work">
+		<div class="site-content">
+			<h4>Featured Work</h4>
+			<ul class="homepage-featured-work">
+				<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
+				<!-- the loop -->
+				<?php while ( have_posts() ) : the_post();
+						$image_1 = get_field("image_1");
+						$size = "medium"; ?>
+
+						<li class="individual-featured-work">
+							<a href="<?php the_permalink(); ?>">
+								<figure>
+									<?php echo wp_get_attachment_image($image_1, $size); ?>
+								</figure>
+								<h3><?php the_title(); ?></h3>
+							</a>
+						</li>
+					<?php endwhile; // end of the loop. ?>
+				<?php wp_reset_query(); // resets the altered query back to the original ?>
+			</ul>
+		</div>
+	</section>
+
 	<!-- RECENT BLOG POST -->
 	<section class="recent-posts">
 		<div class="site-content">
