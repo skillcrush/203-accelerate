e3d59b2de3907ad3472f97ff76279f5d55e78f3c
Maren Vernon <marenvernon@gmail.com>
2017-08-29 21:10:23 -0700

    alterations to align with parent theme plus adding frontpage styles and additional images

--- wp-content/themes/accelerate-theme-child/archive-case_studies.php
+++ wp-content/themes/accelerate-theme-child/archive-case_studies.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying the case studies archive page
+ * The template for displaying the case study archive.
  *
  *
  * @package WordPress
@@ -10,18 +10,18 @@
 
 get_header(); ?>
 <div id="primary" class="site-content">
-	<div id="content" role="main">
-	<?php while ( have_posts() ) : the_post(); 
+		<div class="main-content" role="main">
+	<?php while ( have_posts() ) : the_post();
 			$image_1 = get_field("image_1");
 			$size = "full";
 			$services = get_field('services'); ?>
 
-		<article class="case-study clearfix">
+		<article class="case-study">
 			<aside class="case-study-sidebar">
 				<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-				<h5><?php echo $services; ?></h5>
+				<h4><?php echo $services; ?></h4>
 				<?php the_excerpt(); ?>
-				<p><strong><a href="<?php the_permalink(); ?>">View Project</a></strong></p>
+				<p class="read-more-link"><a href="<?php the_permalink(); ?>">View Project &rsaquo;</a></p>
 			</aside>
 			<div class="case-study-images">
 				<a href="<?php the_permalink(); ?>">
@@ -33,7 +33,7 @@ get_header(); ?>
 		</article>
 	<?php endwhile; // end of the loop. ?>
 
-	</div><!-- #content -->
+	</div><!-- .main-content -->
 </div><!-- #primary -->
 
 <?php get_footer(); ?>
--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -14,33 +14,34 @@
 
 get_header(); ?>
 
-<section id="primary" class="home-page hero-content">
+<div id="primary" class="home-page hero-content">
 	<div class="main-content" role="main">
 		<?php while ( have_posts() ) : the_post(); ?>
-			<div class='homepage-hero'>
-				<?php the_content(); ?>
-				<a class="button" href="<?php echo home_url(); ?>/case-studies">View Our Work</a>
-			</div>
+			<?php the_content(); ?>
+				<a class="button" href="<?php echo site_url('/blog/') ?>">View Our Work</a>
 		<?php endwhile; // end of the loop. ?>
 	</div><!-- .main-content -->
-</section><!-- .home-page -->
+</div><!-- #primary -->
 
+<!-- FEATURED WORK -->
 <section class="featured-work">
-	<div class="featured-content">
+	<div class="site-content">
 		<h4>Featured Work</h4>
 
 		<ul class="homepage-featured-work">
 		<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
-			<?php while ( have_posts() ) : the_post(); 
+		<!-- the loop -->
+			<?php while ( have_posts() ) : the_post();
 				$image_1 = get_field("image_1");
 				$size = "medium";
 			?>
 			<li class="individual-featured-work">
-				<figure>
-					<?php echo wp_get_attachment_image($image_1, $size); ?>
-				</figure>
-
-				<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+				<a href="<?php the_permalink(); ?>">
+					<figure>
+						<?php echo wp_get_attachment_image($image_1, $size); ?>
+					</figure>
+					<h3><?php the_title(); ?></h3>
+				</a>
 			</li>
 			<?php endwhile; // end of the loop. ?>
 			<?php wp_reset_query(); // resets the altered query back to the original ?>
@@ -49,15 +50,17 @@ get_header(); ?>
 	</div>
 </section>
 
+<!-- RECENT BLOG POST -->
 <section class="recent-posts">
 	<div class="site-content">
 		<div class="blog-post">
 			<h4>From the Blog</h4>
 			<?php query_posts('posts_per_page=1'); ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-				<h2><?php the_title(); ?></h2>
-				<?php the_excerpt(); ?>
-			<?php endwhile; // end of the loop. ?>
+				<!-- the loop -->
+				<?php while ( have_posts() ) : the_post(); ?>
+					<h3><?php the_title(); ?></h3>
+					<?php the_excerpt(); ?>
+				<?php endwhile; // end of the loop. ?>
 			<?php wp_reset_query(); // resets the altered query back to the original ?>
 		</div>
 	</div>
