44934c05316ff26266a957ad78635e64ce420a36
Maren Vernon <marenvernon@gmail.com>
2017-08-29 20:27:21 -0700

    a few alterations to align with parent theme changes

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -1,53 +1,57 @@
 <?php
 /**
- * The template for displaying case studies
+ * The template for displaying a single case study.
  *
  *
  * @package WordPress
- * @subpackage Accelerate_Theme
- * @since Accelerate Theme 2.0
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
  */
 
 get_header(); ?>
 
 <div id="primary" class="site-content">
-	<div id="content" role="main">
+	<div class="main-content" role="main">
 
-	<?php while ( have_posts() ) : the_post(); 
-		$services = get_field('services'); 
+	<?php while ( have_posts() ) : the_post();
+		$services = get_field('services');
 		$client = get_field('client');
 		$link = get_field('site_link');
 		$image_1 = get_field('image_1');
 		$image_2 = get_field('image_2');
-		$image_3 = get_field('image_3'); 
+		$image_3 = get_field('image_3');
 		$size = "full";
 		?>
 
 	<article class="case-study">
 		<aside class="case-study-sidebar">
 			<h2><?php the_title(); ?></h2>
-			<h5><?php echo $services; ?></h5>
-			<h6>Client: <?php echo $client; ?></h6>
+			<h4><span><?php echo $services; ?></span></h4>
+			<h4>Client: <?php echo $client; ?></h4>
 
 			<?php the_content(); ?>
 
-			<p><strong><a href="<?php echo $link; ?>">Site Link</a></strong></p>
+			<p class="read-more-link"><a href="<?php echo $site_link; ?>">Visit Live Site &rsaquo;</a></p>
 		</aside>
 
 		<div class="case-study-images">
 			<?php if($image_1) {
 				echo wp_get_attachment_image( $image_1, $size );
 			} ?>
-			<?php if($image_2) { 
+			<?php if($image_2) {
 				echo wp_get_attachment_image( $image_2, $size );
 			} ?>
 			<?php if($image_3) {
 				echo wp_get_attachment_image( $image_3, $size );
 			} ?>
 		</div>
+	</article>
+		<?php endwhile; // end of the loop. ?>
+	</div><!-- .main-content -->
+</div><!-- #primary -->
 
-			</article>
-			<?php endwhile; // end of the loop. ?>
-		</div><!-- #content -->
-	</div><!-- #primary -->
+<div id="navigation" class="container">
+	<div class="left"><a href="<?php echo site_url('/case-studies/') ?>">&larr; <span>Back to Work</span></a></div>
+</div>
+
 <?php get_footer(); ?>
