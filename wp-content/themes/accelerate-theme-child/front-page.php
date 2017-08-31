532600376d8f386a1e79b14d87c0f2e86d141dcb
Maren Vernon <marenvernon@gmail.com>
2017-08-31 13:22:11 -0700

    making homepage featured items with margins not padding

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -25,28 +25,26 @@ get_header(); ?>
 
 <!-- FEATURED WORK -->
 <section class="featured-work">
-	<div class="site-content">
-		<div class="work">
-			<h4>Featured Work</h4>
-			<ul class="homepage-featured-work">
-			<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
-			<!-- the loop -->
-				<?php while ( have_posts() ) : the_post();
-					$image_1 = get_field("image_1");
-					$size = "medium";
-				?>
-				<li class="individual-featured-work">
-					<a href="<?php the_permalink(); ?>">
-						<figure>
-							<?php echo wp_get_attachment_image($image_1, $size); ?>
-						</figure>
-						<h3><?php the_title(); ?></h3>
-					</a>
-				</li>
-				<?php endwhile; // end of the loop. ?>
-				<?php wp_reset_query(); // resets the altered query back to the original ?>
-			</ul>
-		</div>
+	<div class="site-content clearfix">
+		<h4>Featured Work</h4>
+		<ul class="homepage-featured-work">
+		<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
+		<!-- the loop -->
+			<?php while ( have_posts() ) : the_post();
+				$image_1 = get_field("image_1");
+				$size = "medium";
+			?>
+			<li class="individual-featured-work">
+				<a href="<?php the_permalink(); ?>">
+					<figure>
+						<?php echo wp_get_attachment_image($image_1, $size); ?>
+					</figure>
+					<h3><?php the_title(); ?></h3>
+				</a>
+			</li>
+			<?php endwhile; // end of the loop. ?>
+			<?php wp_reset_query(); // resets the altered query back to the original ?>
+		</ul>
 	</div>
 </section>
 
