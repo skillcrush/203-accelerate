87b2207e15ad610689f77dfc62c1d478a1f1f747
Maren Vernon <marenvernon@gmail.com>
2018-02-01 14:57:48 -0800

    change to enqueue method

--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -1,29 +1,21 @@
 <?php
 /**
- * Accelerate Marketing Child functions and definitions
- *
- * Set up the theme and provides some helper functions, which are used in the
- * theme as custom template tags. Others are attached to action and filter
- * hooks in WordPress to change core functionality.
- *
- * When using a child theme you can override certain functions (those wrapped
- * in a function_exists() call) by defining them first in your child theme's
- * functions.php file. The child theme's functions.php file is included before
- * the parent theme's file, so the child theme functions would be used.
- *
- * @link http://codex.wordpress.org/Theme_Development
- * @link http://codex.wordpress.org/Child_Themes
- *
- * Functions that are not pluggable (not wrapped in function_exists()) are
- * instead attached to a filter or action hook.
- *
- * For more information on hooks, actions, and filters,
- * @link http://codex.wordpress.org/Plugin_API
- *
- * @package WordPress
- * @subpackage Accelerate Marketing
- * @since Accelerate Marketing 2.0
- */
+* Accelerate Marketing Child functions and definitions
+*
+* @link http://codex.wordpress.org/Theme_Development
+* @link http://codex.wordpress.org/Child_Themes
+*
+* @package WordPress
+* @subpackage Accelerate Marketing
+* @since Accelerate Marketing 2.0
+*/
+
+// Enqueue scripts and styles
+function accelerate_child_scripts() {
+	wp_enqueue_style( 'accelerate-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'accelerate-style' ));
+}
+add_action( 'wp_enqueue_scripts', 'accelerate_child_scripts' );
 
 // Custom post types function
 function create_custom_post_types() {
@@ -42,25 +34,5 @@ function create_custom_post_types() {
 			)
 	);
 }
-
-// Hook this custom post type function into the theme
+// hook this custom post type function into the theme
 add_action( 'init', 'create_custom_post_types' );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
