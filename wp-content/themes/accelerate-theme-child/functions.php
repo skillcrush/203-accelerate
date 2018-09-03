f50daafc83dbb07de8b4ebffe8ab32cd5635328f
Alex Gray <alexgray987@gmail.com>
2018-09-03 15:56:45 +0100

    created a child theme

---
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* Accelerate Marketing Child functions and definitions
+*
+* @link http://codex.wordpress.org/Theme_Development
+* @link http://codex.wordpress.org/Child_Themes
+*
+* @package WordPress
+* @subpackage Accelerate Marketing
+* @since Accelerate Marketing 2.0
+*/
+
+// Enqueue scripts and styles
+function accelerate_child_scripts(){
+	wp_enqueue_style( 'accelerate-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'accelerate-style' ));
+}
+add_action( 'wp_enqueue_scripts', 'accelerate_child_scripts' );
