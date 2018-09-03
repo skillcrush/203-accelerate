b3f138003b93030c72fb2f8deb2d7b77d4fd2c6b
Alex Gray <alexgray987@gmail.com>
2018-09-03 22:05:51 +0100

    styled the front page post query

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -22,4 +22,18 @@ get_header(); ?>
 		</div><!-- .main-content -->
 	</div><!-- #primary -->
 
+	<section class="recent-posts">
+	 <div class="site-content">
+		<div class="blog-post">
+			<h4>From the Blog</h4>
+			<?php query_posts('posts_per_page=1'); ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+				 <h3><?php the_title(); ?></h3>
+				 <?php the_excerpt(); ?>
+			 <?php endwhile; ?>
+			<?php wp_reset_query(); ?>
+  	</div>
+	 </div>
+	</section>
+
 <?php get_footer(); ?>
