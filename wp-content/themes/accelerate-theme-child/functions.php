95914bd6f8469a281ca8c7cffab2ea269bbe0157
Alex Gray <alexgray987@gmail.com>
2018-09-11 23:18:47 +0100

    created custom post template for case studies

--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -16,3 +16,20 @@ function accelerate_child_scripts(){
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'accelerate-style' ));
 }
 add_action( 'wp_enqueue_scripts', 'accelerate_child_scripts' );
+
+
+function create_custom_post_types() {
+
+    register_post_type( 'case_studies', //new custom post type with a unique name
+        array(
+            'labels' => array( 					//defines settings for your new post type.
+                'name' => __( 'Case Studies' ), //name for your collection of case studies posts. See in the left nav in admin.
+                'singular_name' => __( 'Case Study' ) //name for a single case study post.
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'rewrite' => array( 'slug' => 'case-studies' ), //name used in the URLs for your case study posts.
+        )
+    );
+}
+add_action( 'init', 'create_custom_post_types' );
---
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The template for displaying a single case study.
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 1.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content sidebar">
+		<div class="main-content" role="main">
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php the_content(); ?>
+			<?php endwhile; // end of the loop. ?>
+		</div><!-- .main-content -->
+
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
