00f801ed05e84e49a0b02fb22554b4b02b0ccd0b
Alex Gray <alexgray987@gmail.com>
2018-09-15 14:56:56 +0200

    started to add custom posts onto front page

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -22,6 +22,28 @@ get_header(); ?>
 		</div><!-- .main-content -->
 	</div><!-- #primary -->
 
+	<section class="featured-work">
+		<div class="site-content">
+			<h2>Featured Work</h2>
+			<ul class="homepage-featured-work">
+				<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
+					<?php while ( have_posts() ) : the_post();
+							$image_1 = get_field('image_1');
+							$size = "medium";
+					?>
+					<li class="individual-featured-work">
+							<figure>
+									<?php echo wp_get_attachment_image($image_1, $size); ?>
+							</figure>
+							<h3><a href="<?php the_permalink(); ?>"<?php the_title(); ?></a></h3>
+					</li>
+				 <?php endwhile; ?>
+				<?php wp_reset_query(); /*resets the altered query back to the original*/ ?>
+			</ul>
+		</div>
+	</section>
+
+
 	<section class="recent-posts">
 	 <div class="site-content">
 		<div class="blog-post">
