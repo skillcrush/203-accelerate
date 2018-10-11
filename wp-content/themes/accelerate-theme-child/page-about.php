cece4255f232f1e06bd3326404f5c89ca969e950
Alex Gray <alexgray987@gmail.com>
2018-10-11 15:47:08 +0100

    created custom page file so I can style the page to the brief given

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -1,12 +1,8 @@
 <?php
 /**
- * The template for displaying all pages
+ * The template for displaying the About page
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
- *
+ * 
  * @package WordPress
  * @subpackage Accelerate Marketing
  * @since Accelerate Marketing 2.0
