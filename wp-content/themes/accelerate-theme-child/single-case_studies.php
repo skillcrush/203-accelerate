266a18da76685fde6a99322df38de69bafe264ba
Noel Arzola <noel.arzola@gmail.com>
2020-02-08 13:22:18 -0500

    Styled case-studies posts

---
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying case studies
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content">
+		<div class="main-content" role="main">
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php the_content(); ?>
+			<?php endwhile; // end of the loop. ?>
+		</div><!-- .main-content -->
+
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
