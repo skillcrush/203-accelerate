6b7ba4de218200c6d04484e5d3562e36c0dabbb4
Noel Arzola <noel.arzola@gmail.com>
2020-02-09 01:50:37 -0500

    Added custom fields to case studies

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -16,8 +16,41 @@ get_header(); ?>
 
 	<div id="primary" class="site-content">
 		<div class="main-content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php the_content(); ?>
+			<?php while ( have_posts() ) : the_post();
+				$services = get_field('services');
+				$client = get_field('client');
+				$link = get_field('site_link');
+				$image_1 = get_field('image_1');
+				$image_2 = get_field('image_2');
+				$image_3 = get_field('image_3');
+			?>
+	<article class="case-study">
+		<aside class="case-study-sidebar">
+			<h2><?php the_title(); ?></h2>
+			<h3><span><?php echo $services; ?></span></h3>
+			<h3>Client: <?php echo $client; ?></h3>
+
+			<?php the_content(); ?>
+
+			<p><a href="<?php echo $link; ?>"></a></p>
+			
+			<p class="read-more-link"><a href="<?php echo $link; ?>">Visit Live Site &rsaquo;</a></p>
+		</aside>
+		
+		<div class="case-study-images">
+			<?php if($image_1) { ?>
+				<img src="<?php echo $image_1; ?>" >
+			<?php } ?>
+			<?php if($image_2) { ?>
+				<img src="<?php echo $image_2; ?>" >
+			<?php } ?>
+			<?php if($image_3) { ?>
+				<img src="<?php echo $image_3; ?>" >
+			<?php } ?>
+		</div>
+	</article>
+
+				
 			<?php endwhile; // end of the loop. ?>
 		</div><!-- .main-content -->
 
