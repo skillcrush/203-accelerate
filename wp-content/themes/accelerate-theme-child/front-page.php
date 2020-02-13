fd3f3b605588612495abad95a5b8923b6c96738e
Noel Arzola <noel.arzola@gmail.com>
2020-02-13 09:36:06 -0500

    Added case studies to the home page

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -23,12 +23,38 @@ get_header(); ?>
 		</div><!-- .main-content -->
 	</div><!-- #primary -->
 
+	<section class="featured-work">
+		<div class="site-content">
+			<h4>Featured Work</h4>
+			<ul class="homepage-featured-work">
+				<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
+					<?php while ( have_posts() ) : the_post();
+						$image_1 = get_field('image_1');
+						$size = "medium";
+					?>
+						<li class="individual-featured-work">
+							<figure>
+								<a href="<?php the_permalink(); ?>">
+									<?php echo wp_get_attachment_image($image_1, $size); ?>
+								</a>
+							</figure>
+
+							<h3>
+								<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+							</h3>
+						</li>
+					<?php endwhile; ?>
+				<?php wp_reset_query(); ?>
+			</ul>
+		</div>
+	</section>
+
 	<section class="recent-posts">
 		<div class="site-content">
 			<div class="blog-post">
 				<h4>From The Blog</h4>
 				<?php query_posts('posts_per_page=1'); ?>
-					<?php while (have_posts()) : the_post(); ?>
+					<?php while ( have_posts() ) : the_post(); ?>
 						<h3><?php the_title(); ?></h3>
 						<?php the_excerpt(); ?>
 					<?php endwhile; ?>
