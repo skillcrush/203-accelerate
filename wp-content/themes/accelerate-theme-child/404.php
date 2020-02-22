7d9e8e627287a02a8a48332e9a811474e1fa05db
Noel Arzola <noel.arzola@gmail.com>
2020-02-22 14:41:57 -0500

    Added 404 page

---
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The template for displaying the 404 Page
+ *
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
+ */
+
+get_header(); ?>
+
+	<div class="error-page-text">
+		<h1>There is a fifth dimension beyond that which is known to man.<br>It is a dimension as vast as space and as timeless as infinity. <br>It is the middle ground between light and shadow, between science and superstition, and it lies between the pit of man's fears and the summit of his knowledge. <br>This is the dimension of imagination. <br>It is an area which we call: <br>The 404 Zone.</h1>
+		<a href="<?php echo site_url('#') ?>">&larr; Escape The 404 Zone!!!</a>
+	</div>
+
+<?php get_footer(); ?>
