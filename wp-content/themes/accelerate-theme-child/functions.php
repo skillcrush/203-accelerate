2dc1dec124a57a085d8233ac92ede8a5613bc721
Lisa Savoie <twosavoie@gmail.com>
2025-07-16 13:16:29 -0500

    Update functions.php file's formatting

--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -12,7 +12,7 @@
 
 // Enqueue scripts and styles
 function accelerate_child_scripts() {
-	wp_enqueue_style( 'accelerate-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'accelerate-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'accelerate-style' ));
 }
 add_action( 'wp_enqueue_scripts', 'accelerate_child_scripts' );
